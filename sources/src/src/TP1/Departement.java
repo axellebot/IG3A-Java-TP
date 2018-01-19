d7e8091ac7cc4e1deabf49924e755ad1b3d6703f
BOT_Axel <axellebot@gmail.com>
2018-01-19 16:30:24 +0100

    [FEAT] Added TP1 sources
    - Edited gitignore

---
+++ sources/src/src/TP1/Departement.java
@@ -0,0 +1,80 @@
+package TP1;
+
+public class Departement {
+    private Ville[] tabVilles;
+    private int numero;
+    private String nom;
+    private int nbVillesSaisies;
+
+    /**
+     * Constructor
+     *
+     * @param nom
+     * @param numero
+     * @param nbVilles
+     */
+    public Departement(String nom, int numero, int nbVilles) {
+        this.nom = nom;
+        this.numero = numero;
+        this.nbVillesSaisies = 0;
+        tabVilles = new Ville[nbVilles];
+    }
+
+    void ajouterVille(Ville ville) {
+        if (tabVilles.length > nbVillesSaisies) {
+            tabVilles[nbVillesSaisies] = ville;
+            nbVillesSaisies++;
+        } else {
+            System.out.println("Tableau plein");
+        }
+    }
+
+
+    public Ville[] getTabVilles() {
+        return tabVilles;
+    }
+
+    public void setTabVilles(Ville[] tabVilles) {
+        this.tabVilles = tabVilles;
+    }
+
+    public int getNumero() {
+        return numero;
+    }
+
+    public void setNumero(int numero) {
+        this.numero = numero;
+    }
+
+    public String getNom() {
+        return nom;
+    }
+
+    public void setNom(String nom) {
+        this.nom = nom;
+    }
+
+    public int getNbVillesSaisies() {
+        return nbVillesSaisies;
+    }
+
+    public void setNbVillesSaisies(int nbVillesSaisies) {
+        this.nbVillesSaisies = nbVillesSaisies;
+    }
+
+
+    @Override
+    public String toString() {
+        String msg = "";
+
+        Ville tmp = null;
+        msg += "Ville du Departement " + this.nom + "(" + this.numero + "):\n";
+
+        for (int i = 0; i < tabVilles.length; i++) {
+            tmp = tabVilles[i];
+            msg += i+1 + ". " + tmp + '\n';
+        }
+        return msg;
+
+    }
+}
